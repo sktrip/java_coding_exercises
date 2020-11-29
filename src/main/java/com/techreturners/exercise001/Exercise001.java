08a5d20dde932ab9d5fad3122588f19f8152870f
sktrip <sktripurari@gmail.com>
2020-11-29 22:47:18 +0000

    exercise 001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -3,25 +3,37 @@ package com.techreturners.exercise001;
 import java.util.List;
 
 public class Exercise001 {
-    public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+    public String capitalizeWord(String word) {         
+            String words[]= word.split("\\s");  
+            String capitalizeWord="";  
+            for(String w:words){  
+                String first=w.substring(0,1);  
+                String afterfirst=w.substring(1);  
+                capitalizeWord += first.toUpperCase()+afterfirst+" ";  
+            }  
+            return capitalizeWord.trim();  
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        return firstName.substring(0,1)+"."+lastName.substring(0,1);
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        double vatAmount = (originalPrice * vatRate)/100;
+        double roundedValue = Math.round((originalPrice+vatAmount)*100)/100.0;
+        return roundedValue;
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
-    }
+        if (sentence.isEmpty())
+            return sentence;
+
+        return reverse(sentence.substring(1)) + sentence.charAt(0);
+  }
+ 
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
