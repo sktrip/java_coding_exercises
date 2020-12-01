784f293076d42c3f3e0ba9e56f831221eccac4ce
sktrip <sktripurari@gmail.com>
2020-12-01 18:26:56 +0000

    Exercise001-Completed

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -3,41 +3,34 @@ package com.techreturners.exercise001;
 import java.util.List;
 
 public class Exercise001 {
-    public String capitalizeWord(String word) {         
-        return Character.toUpperCase(word.charAt(0))+word.substring(1);
+    public String capitalizeWord(String word) {
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return firstName.substring(0,1)+"."+lastName.substring(0,1);
+        return firstName.substring(0, 1) + "." + lastName.substring(0, 1);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        double vatAmount = (originalPrice * vatRate)/100;
-        double roundedValue = Math.round((originalPrice+vatAmount)*100)/100.0;
+        double vatAmount = (originalPrice * vatRate) / 100;
+        double roundedValue = Math.round((originalPrice + vatAmount) * 100) / 100.0;
         return roundedValue;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
         if (sentence.isEmpty())
             return sentence;
-
         return reverse(sentence.substring(1)) + sentence.charAt(0);
-  }
- 
+    }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        int count =0;
-        for (int i=0; i<users.size(); i++) {
-            User systemName = users.get(i) ;
-            if(systemName.getType().equals("Linux")) {
+        int count = 0;
+        for (int i = 0; i < users.size(); i++) {
+            User systemName = users.get(i);
+            if (systemName.getType().equals("Linux")) {
                 count++;
             }
-            
-            }  return count;
         }
-        
+        return count;
     }
+}
