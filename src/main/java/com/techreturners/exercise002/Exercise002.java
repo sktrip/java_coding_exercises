082b29e71e3b7950780c149b1ebea041545f27e0
sktrip <sktripurari@gmail.com>
2020-12-01 18:39:14 +0000

    Update Exercise002.java

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,11 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester( Person person ) {
-        // Add your code here
+    public boolean isFromManchester(Person person) {
         return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-        // Add your code here
-           return person.getAge() == ageLimit;    
+        return person.getAge() == ageLimit;
     }
-    
 }
