f79bc471413387eefec6059b18a394ef5aea25e0
sktrip <74243504+sktrip@users.noreply.github.com>
2020-12-01 18:45:35 +0000

    Exercise002-Completed

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,12 +3,10 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester( Person person ) {
-        // Add your code here
         return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-        // Add your code here
            return person.getAge() == ageLimit;    
     }
     
